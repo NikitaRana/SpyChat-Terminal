282c948d2697b4369241a832f4658d177e11b785
Nikita Rana <32420201+NikitaRana@users.noreply.github.com>
2018-03-21 12:19:09 +0530

    Spy Welcome and Salutation

---
+++ Main.py
@@ -0,0 +1,10 @@
+print('Spy Chat')
+print('Let\'s get started')
+spy_name = raw_input("What is your spy name?")
+print('Welcome'+spy_name+'How are you?')
+
+# to check the datatype of the entered spy name
+type(spy_name)
+
+spy_salutation = raw_input("What should I call you Mr. or Ms. ?")
+print('Alright '+spy_salutation+'.'+spy_name+' I\'d like to know a little bit more about you...')
