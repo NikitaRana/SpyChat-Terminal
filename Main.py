d84ee26617f585e6f30850db9e6a41e5554c6c9a
Nikita Rana <32420201+NikitaRana@users.noreply.github.com>
2018-03-26 14:49:17 +0530

    Added menu options and file import function

--- Main.py
+++ Main.py
@@ -4,22 +4,49 @@
 #-------------------------------------------------------------------------------------------------
 # Code for lecture 1 of Spychat dated 19th March 2018
 #-------------------------------------------------------------------------------------------------
-print('Spy Chat')
-print('Let\'s get started')
-spy_name = raw_input("What is your spy name?") #to store the name of the spy
-print('Welcome '+spy_name+'. How are you?')
-
-# to check the datatype of the entered spy name
-type(spy_name)
-
-spy_salutation = raw_input("What should I call you Mr. or Ms. ?") #salutation for the spyname
-print('Alright '+spy_salutation+'.'+spy_name+' I\'d like to know a little bit more about you...')
+# print('Spy Chat')
+# print('Let\'s get started')
+# spy_name = raw_input("What is your spy name?") #to store the name of the spy
+# print('Welcome '+spy_name+'. How are you?')
+# # to check the datatype of the entered spy name
+# type(spy_name)
+# spy_salutation = raw_input("What should I call you Mr. or Ms. ?") #salutation for the spyname
+# print('Alright '+spy_salutation+'.'+spy_name+' I\'d like to know a little bit more about you...')
 
 #-------------------------------------------------------------------------------------------------
 # Code for lecture 2 of Spychat dated 21st March 2018
 #-------------------------------------------------------------------------------------------------
+from Spy_details import Default_user #calling default user details
+def start_chat(spy_name, spy_age, spy_rating): #currently not using the parameters
+    continue_option = "Y"
+    while(continue_option=='Y' or continue_option=='y'):
+        menu_option = int ( raw_input ("What would you like to do \n 1. Add a status update \n 2. Add a friend \n 3. Send a secret message \n 4. Read a secret message \n 5. Read chats from a user \n 6. Close the application" ) )
+        while(menu_option<=6):
+            if menu_option==1:
+                print("Status update")
+                break
+            elif menu_option==2:
+                print("Adding a friend initiated......")
+                break
+            elif menu_option==3:
+                print("Send a secret message initiated......")
+                break
+            elif menu_option==4:
+                print("Read a secret message initiated......")
+                break
+            elif menu_option==5:
+                print("Reading chat from user")
+                break
+            else:
+                print("Exiting now.....")
+        continue_option = raw_input("Would you like to perform another operation (Y/N)")
+    print("Thank you for your time")
+
 spy_is_online = False #status of the spy
 user_option = raw_input("Would you like to continue as a default user (default) or create your own (new)? ") #type of user
+#-------------------------------------------------------------------------------------------------
+# for creating new user
+#-------------------------------------------------------------------------------------------------
 if user_option == "new":
     spy_name = raw_input("Welcome to SpyChat, you must tell me you Spyname first:" )
     if len ( spy_name ) > 0: #to calculate the length of the string
@@ -41,11 +68,17 @@ if user_option == "new":
             print('We can always use somebody to help in the office. ')
     else:
     print('Sorry you are not of the correct age to become a spy.') #entered age is not between 12 and 50
-    print('Authentication Complete. We are glad to have you with us. Welcome '+spy_salutation+'.'+spy_name+","Having age "+str(spy_age)+".Your spy rating is "+str(spy_rating)) #float value to string value
+    print('Authentication Complete. We are glad to have you with us. Welcome '+spy_salutation+'.'+spy_name+", Your sp rating is "+str(spy_rating)) #float value to string value
     spy_is_online = True
     print('Changing the status of spy from offline to online '+str(spy_is_online)) #bool value to string value for concatenation
+    start_chat(spy_name,spy_age,spy_rating) # calling menu option
+#-----------------------------------------------------------------------------------------------------------------------
+# for continuing as a default user
+#-----------------------------------------------------------------------------------------------------------------------
 elif user_option=='default':
-    print("import spy_details.py objective not covered yet")
+    print('Authentication Complete. We are glad to have you with us. Welcome ' + Default_user().spy_salutation + '.' + Default_user().spy_name + ", Your sp rating is " + str(Default_user().spy_rating))  # float value to string value
+    spy_is_online = True
+    start_chat(Default_user().spy_name,Default_user().spy_age,Default_user().spy_rating) # calling menu option
 else:
     print("Please select default user or create a new one.")
 
---
+++ Spy_details.py
@@ -0,0 +1,9 @@
+#------------------------------------------------------------------------------------
+#  The Spy_details.py file is meant for a Default user and shall be imported when the
+# user is given the option to continue as a default user or create a new spy user.
+#------------------------------------------------------------------------------------
+def Default_user():
+    spy_name = 'SpyId_007'
+    spy_rating = 4.5
+    spy_age = 34
+    spy_salutation = 'mr'
